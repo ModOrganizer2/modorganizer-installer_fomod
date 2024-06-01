01996cc666beb1b36117370ce19f3c92aeb77459
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-01 16:11:38 +0200

    Refactoring following uibase change for game features.

--- src/fomodinstallerdialog.cpp
+++ src/fomodinstallerdialog.cpp
@@ -471,7 +471,7 @@ std::pair<bool, QString> FomodInstallerDialog::testCondition(int, const VersionC
     break;
 
   case VersionCondition::v_FOSE: {
-    ScriptExtender* extender = m_MoInfo->gameFeatures()->gameFeature<ScriptExtender>();
+    auto extender = m_MoInfo->gameFeatures()->gameFeature<ScriptExtender>();
     if (extender != nullptr) {
       version = extender->getExtenderVersion();
       typeName = extender->BinaryName();
